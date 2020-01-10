94a572051d3e5c9a0a89e7d416d01f1b76371cc9
Tyler Moretto <morettty@shernet.sheridancollege.ca>
2020-01-10 13:46:56 -0500

    Initial Unity Commit

---
+++ TestProject/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string name = "Nick";
+    public int health = 200;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(name);
+    }
+}
