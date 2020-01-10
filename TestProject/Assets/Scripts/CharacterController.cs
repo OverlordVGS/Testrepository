5221e33a21bbc6826e844eaa367e64db90576c84
Tyler Moretto <morettty@shernet.sheridancollege.ca>
2020-01-10 13:58:10 -0500

    Updated character controller, changed name and removed print

--- TestProject/Assets/Scripts/CharacterController.cs
+++ TestProject/Assets/Scripts/CharacterController.cs
@@ -6,7 +6,7 @@ public class CharacterController : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public string name = "Nick";
+    public string name = "Cool Dude Nick";
     public int health = 200;
     void Start()
     {
@@ -16,6 +16,6 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print(name);
+    
     }
 }
